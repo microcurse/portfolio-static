420b094051fd4b0167d69fb921ad67de4a35272c
Marc <marc.maninang@gmail.com>
2020-04-01 12:56:36 -0700

    started function for showing nav

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/** Responsive Menu */
+
+function showNav() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.add('show');
+}
+
+
+//showNav();
+
+
+
+console.log(navLinks);
