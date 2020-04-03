d56a64eb7193e84fdc1d3c4a057b5cad38c2511e
Marc <marc.maninang@gmail.com>
2020-04-03 09:17:56 -0700

    refactor

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 /** Responsive Menu Button */
 
-const showNav = function() {
+document.querySelector('.nav-button').addEventListener('click', function () {
   const navLinks = document.querySelector('.nav-links');
   navLinks.classList.add('show');
-}
-
-document.querySelector('.nav-button').addEventListener('click', showNav);
+});
